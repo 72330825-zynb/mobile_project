976149ef46c07fe512b46be1c955fd239a6805d0
72330825-zynb <72330825@github.com>
2026-01-03 08:44:32 +0200

    Its done zynb

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 // Routes
-const loginRoute = require("./routes/login")
+const loginRoute = require("./routes/login");
 const areasRouter = require("./routes/areas");
 const categoriesRouter = require("./routes/categories");
 const placesRouter = require("./routes/places");
@@ -18,29 +19,22 @@ const finalRoute = require("./routes/final");
 
 // Root
 app.get("/", (req, res) => {
-  res.send(
-    "🚀 Mobile Project API is running! Use /items, /places, /heritage, /destination"
-  );
+  res.send("🚀 Mobile Project API is running!");
 });
-
-router.post("/", (req, res) => {
-  console.log("BODY:", req.body);
-  res.json({ ok: true });
-});
-
-
-
 
 // Use Routes
-app.use("/login" , loginRoute);
+app.use("/login", loginRoute);
 app.use("/items", areasRouter);
 app.use("/categories", categoriesRouter);
 app.use("/places", placesRouter);
 app.use("/destination", destinationRouter);
 app.use("/heritage", heritageRouter);
-app.use("/details_her" , heritageDetailsRouter);
-app.use("/final" , finalRoute);
+app.use("/details_her", heritageDetailsRouter);
+app.use("/final", finalRoute);
 
 // Start Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on port ${PORT}`)
+);
+
